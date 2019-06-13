6a12bb3bf7191bd3430cbf6062f797618d322006
unknown <sharath.pvs@whiznext.com>
2019-06-13 14:28:11 +0530

    District category fixed

--- public/controllers/reports.js
+++ public/controllers/reports.js
@@ -41,11 +41,7 @@ mainapp.controller('reportController', ['$scope',
     
       $scope.print = function(printSectionId) {
           $scope.printButton = false;
-        var innerContents = document.getElementById(printSectionId).innerHTML;
-        var popupWinindow = window.open('', '_blank', 'width=600,height=700,scrollbars=no,menubar=no,toolbar=no,location=no,status=no,titlebar=no');
-        popupWinindow.document.open();
-        popupWinindow.document.write('<html><head><link rel="stylesheet" type="text/css" href="style.css" /></head><body onload="window.print()">' + innerContents + '</html>');
-        popupWinindow.document.close();
+        window.print();
       }
    
   
